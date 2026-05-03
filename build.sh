87e831bb9f1b06efae79079df797e783d446caa7
Kirill Fedkin <kirill256823@yandex.com>
2026-05-04 00:26:50 +0300

    Add build log messages

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
+
+echo "Applying migrations..."
 python manage.py migrate
 
+echo "Importing products..."
 python manage.py import_products products_ru_500.csv
+
+echo "Creating or updating admin user..."
 python manage.py create_admin
+
+echo "Build finished successfully."
